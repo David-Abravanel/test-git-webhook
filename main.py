50a98c2cca95bdd578077f2ee346bca3a636655e
daivid-1 <daivid.aba@gmail.com>
2024-12-09 16:14:39 +0200

    9

--- main.py
+++ main.py
@@ -139,7 +139,7 @@ async def github_webhook(request: Request):
 
 @app.post("/test")
 async def github_webhook(request: Request):
-    return 8
+    return 9
 
 # if __name__ == "__main__":
 #     """
